804993a6c158e96dbe624ae6e5efd3884580bb67
yarr <yarr@debian-yaroslav.localdomain>
2014-10-31 18:25:22 +0400

    new files

---
+++ algo.hpp
@@ -0,0 +1,14 @@
+
+#include <vector>
+#include <string>
+
+#include <cassert>
+
+
+
+class CAhoCorasick {
+	CAhoCorasick (const std::vector <std::string> & arrPattaren);
+	CAhoCorasick (const std::string & pattern);
+	
+	
+};
---
+++ main.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <fstream>
+
+#include <cerrno>
+#include <cstring>
+
+#include "algo.hpp"
+
+
+
+int main (int argc, char **argv) {
+	std::string tstString = "qwerty123456789zxcvbnm";
+	std::vector <std::string> tstSubsArr = {"ert", "2345", "987654", "bnm"};
+	
+	
+	std::cout << "=========\n";
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
