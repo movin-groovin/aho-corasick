3545f6bd37d2299c2819229e50375617611e39b1
yarr <yarr@debian-yaroslav.localdomain>
2014-11-07 18:32:18 +0400

    some code

--- algo.hpp
+++ algo.hpp
@@ -6,9 +6,45 @@
 
 
 
-class CAhoCorasick {
-	CAhoCorasick (const std::vector <std::string> & arrPattaren);
-	CAhoCorasick (const std::string & pattern);
-	
-	
+const size_t ALPHABET_SIZE = 256; // everything ascii symbols
+
+
+
+class CNode {
+private:
+	//
+public:
+	//
 };
+
+
+template <size_t Alphabet = ALPHABET_SIZE>
+class CAhoCorasick {
+public:
+	CAhoCorasick (const std::vector <std::string> & arrPatterns):
+		m_Patterns (arrPatterns)
+	{
+		//
+	}
+	
+private:
+	const std::vector <std::string> m_Patterns;
+	CNode * m_treeRoot;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
