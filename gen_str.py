e7bdb025aa9d798e33bcd8f11052c86aa43bcc21
yarr <yarr@debian>
2014-12-11 12:37:18 +0000

    1 gb test

---
+++ gen_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+
+import sys
+import os
+
+
+
+def main():
+	print "========"
+	return 0
+
+
+
+
+if __name__ == '__main__':
+	main()
+else:
+	raise RuntimeError ("Can't run like a module")
